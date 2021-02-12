16d9f679f22c195a89d05828cdcc18aa4a228baa
nayankyada <kayadanayan@gmail.com>
2021-02-12 09:41:57 +0530

    header Done header Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
+import Header from "./Header";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Switch>
+      <Route path="/checkout">
+        {}
+        <h1>Checkout</h1>
+      </Route>
+      <Route path="/login">
+        <h1>login</h1>
+      </Route>
+      <Route path="/">
+        <Header />
+      </Route>
+    </Switch>
   );
 }
