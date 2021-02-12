b3d1fd4b350907eb427415b18e73d1b30854e4b3
nayankyada <kayadanayan@gmail.com>
2021-02-12 10:53:39 +0530

    Home Done Home Done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 export default function App() {
   return (
     <Switch>
       <Route path="/checkout">
-        {}
+        <Header />
         <h1>Checkout</h1>
       </Route>
       <Route path="/login">
+        <Header />
         <h1>login</h1>
       </Route>
       <Route path="/">
         <Header />
+        <Home />
       </Route>
     </Switch>
   );
